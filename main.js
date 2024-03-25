e87901bf8a0129921e3574b7525ad6fa23ee4bd9
zainab <olohundarekeji@gmail.com>
2024-03-25 14:22:08 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+
+    function IndividualProfileToggler(){
+        document.getElementById("individualProfile").classList.add("active");
+        document.getElementById("businesProfile").classList.remove("active");
+    }
+    function BusinesProfileToggler(){
+        document.getElementById("businesProfile").classList.add("active");
+        document.getElementById("individualProfile").classList.remove("active");
+    }
